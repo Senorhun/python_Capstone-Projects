66e49960e90da4cb82413153c2e463c2d7d2c856
Senorhun <petersz.andrassy@gmail.com>
2025-04-26 21:16:52 +0200

    feat: pong game paddle movement

--- Pong_game/main.py
+++ Pong_game/main.py
@@ -1,38 +1,21 @@
-from turtle import Screen, Turtle
-
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_right():
-    new_x = paddle.xcor() + 20
-    paddle.goto(new_x, paddle.ycor())
-
-def go_left():
-    new_x = paddle.xcor() - 20
-    paddle.goto(new_x, paddle.ycor())
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
-screen.onkey(go_right, "Right")
-screen.onkey(go_left, "Left")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s ")
 
-screen.exitonclick()
+game_is_on = True
+while game_is_on:
+    screen.update()
