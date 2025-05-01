0e19f6478670be6ae92bb52fca764ef34c8f13f4
Senorhun <petersz.andrassy@gmail.com>
2025-05-01 02:32:19 +0200

    feat: pong game ball speeds up

--- Pong_game/ball.py
+++ Pong_game/ball.py
@@ -9,6 +9,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 5
         self.y_move = 5
+        self.move_speed = 0.05
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -20,8 +21,10 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.8
 
     def reset_position(self):
+        self.move_speed = 0.05
         self.goto(0,0)
         self.bounce_x()
     
