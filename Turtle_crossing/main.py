8b61891e1a43c21c64d1b0feb9b051455d294659
Senorhun <petersz.andrassy@gmail.com>
2025-05-05 12:39:44 +0200

    feat: add turtle crossing

---
+++ Turtle_crossing/main.py
@@ -0,0 +1,14 @@
+from turtle import Screen
+import time
+from player import Player
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+game_is_on = True
+screen.exitonclick()
+turtle = Player()
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
