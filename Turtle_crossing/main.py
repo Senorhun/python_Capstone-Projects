48b2bcbef5ab21f82294e64beeb5664b513098a2
Senorhun <petersz.andrassy@gmail.com>
2025-05-06 20:03:12 +0200

    feat: turtle crossing add car obj

--- Turtle_crossing/main.py
+++ Turtle_crossing/main.py
@@ -1,12 +1,14 @@
 from turtle import Screen
 import time
 from player import Player
+from car_manager import CarManager
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 game_is_on = True
 turtle = Player()
-
+car = CarManager()
 screen.listen()
 screen.onkey(turtle.move_up, "Up")
 
