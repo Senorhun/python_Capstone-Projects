acb2ce9dc5245370eb9698f3a86c26516d65eba5
Senorhun <petersz.andrassy@gmail.com>
2025-05-09 21:07:59 +0200

    feat: turtle crossing add scoreboard + game over

--- Turtle_crossing/main.py
+++ Turtle_crossing/main.py
@@ -2,7 +2,7 @@ from turtle import Screen
 import time
 from player import Player
 from car_manager import CarManager
-
+from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
@@ -11,16 +11,23 @@ turtle = Player()
 car = CarManager()
 screen.listen()
 screen.onkey(turtle.move_up, "Up")
+scoreboard = Scoreboard()
 
 while game_is_on:
     time.sleep(0.1)
     screen.update()
     car.create_car()
     car.move()
+    scoreboard.scoreboard_update()
     for c in car.car_list:
         if abs(turtle.xcor() -c.xcor()) < 28 and abs(turtle.ycor() - c.ycor()) <20:
+            scoreboard.game_over()
             game_is_on = False
     if turtle.ycor() > 280:
         turtle.next_lvl()
         car.speed_up()
+        scoreboard.score_up()
+        scoreboard.scoreboard_update()
+
+
 screen.exitonclick()
--- Turtle_crossing/scoreboard.py
+++ Turtle_crossing/scoreboard.py
@@ -1,4 +1,23 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.lvl = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280,250)
+
+    def scoreboard_update(self):
+        self.clear()
+        self.write(f"Level: {self.lvl}", align='left', font= FONT)
+
+    def score_up(self):
+         self.lvl += 1
+    
+    def game_over(self):
+        self.goto(0, 0)
+        self.clear()
+        self.write(f"GAME OVER", align='center', font= FONT)
