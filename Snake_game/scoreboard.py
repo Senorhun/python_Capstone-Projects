8c11a3f89f1f93fe4d16edf0579223c7780e8440
Senorhun <petersz.andrassy@gmail.com>
2025-05-10 17:52:20 +0200

    refactor: snake game scoreboard counts lives

--- Snake_game/scoreboard.py
+++ Snake_game/scoreboard.py
@@ -8,14 +8,19 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.best_score = 0
+        self.lives = 3
         
         self.refresh_scoreboard()
         self.hideturtle()
 
     def game_over(self):
-        self.goto(0,0)
-        self.color("white")
-        self.write("Game over", align="center", font=FONT)
+        
+        self.lives -= 1
+        self.refresh_scoreboard()
+        if self.lives == 0:
+            self.color("white")
+            self.goto(0,0)
+            self.write("GAME OVER", align="center", font=FONT)
 
     def increase_score(self):
         self.score += 1
@@ -28,6 +33,8 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 250)
         self.write(f"Score: {self.score}   Best score: {self.best_score}", align="center", font=FONT)
+        self.goto(0, 200)
+        self.write(f"{self.lives} Lives left", align="center", font=FONT)
 
     def reset_score(self):
         if self.score > self.best_score:
