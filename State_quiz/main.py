21540d6457fc2bef0bc9a887bb225b12f26ebeab
Senorhun <petersz.andrassy@gmail.com>
2025-05-22 00:23:00 +0200

    feat: state_quiz input marks map

--- State_quiz/main.py
+++ State_quiz/main.py
@@ -3,7 +3,7 @@ import turtle
 
 data = pandas.read_csv("State_quiz/50_states.csv")
 data_list = data["state"].to_list()
-print(data_list)
+
 
 screen = turtle.Screen()
 image = "State_quiz/blank_states_img.gif"
@@ -15,12 +15,15 @@ guessed_states = []
 text = turtle.Turtle()
 text.hideturtle()
 text.penup()
-text.goto(0,-200)
 
-for i in range(2):
-    user_input = screen.textinput(title=f"Guessing states {len(guessed_states)}/50", prompt="Guess state's name: ")
+for i in range(len(data_list)):
+    user_input = screen.textinput(title=f"Guessing states {len(guessed_states)}/50", prompt="Guess state's name: ").capitalize()
     if user_input in data_list:
-        text.write("Success")
+        state_data = data[data["state"] == user_input]
+        text_x = int(state_data["x"])
+        text_y = int(state_data["y"])
+        text.goto(text_x,text_y)
+        text.write(user_input, align="center", font=("Arial", 10, "normal"))
         guessed_states.append(user_input)
 
 screen.exitonclick()
