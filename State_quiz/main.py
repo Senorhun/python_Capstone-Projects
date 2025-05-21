9660b979da4ff5ae93fbf181e1cd57e4037fcdd5
Senorhun <petersz.andrassy@gmail.com>
2025-05-21 23:50:12 +0200

    feat: state_quiz screen + input

--- State_quiz/main.py
+++ State_quiz/main.py
@@ -1,2 +1,12 @@
 import pandas
+import turtle
 
+data = pandas.read_csv("State_quiz/50_states.csv")
+
+
+screen = turtle.Screen()
+image = "State_quiz/blank_states_img.gif"
+screen.bgpic(image)
+screen.title("USA states quiz game")
+screen.textinput(title="Guessing states", prompt="Guess state's name: ")
+screen.exitonclick()
