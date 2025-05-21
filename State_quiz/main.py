ad0f4635ebe2cccd73cfa1471d096da32ace9804
Senorhun <petersz.andrassy@gmail.com>
2025-05-21 23:56:15 +0200

    feat: state_quiz input check

--- State_quiz/main.py
+++ State_quiz/main.py
@@ -2,11 +2,25 @@ import pandas
 import turtle
 
 data = pandas.read_csv("State_quiz/50_states.csv")
-
+data_list = data["state"].to_list()
+print(data_list)
 
 screen = turtle.Screen()
 image = "State_quiz/blank_states_img.gif"
 screen.bgpic(image)
 screen.title("USA states quiz game")
-screen.textinput(title="Guessing states", prompt="Guess state's name: ")
+
+guessed_states = []
+
+text = turtle.Turtle()
+text.hideturtle()
+text.penup()
+text.goto(0,-200)
+
+for i in range(2):
+    user_input = screen.textinput(title=f"Guessing states {len(guessed_states)}/50", prompt="Guess state's name: ")
+    if user_input in data_list:
+        text.write("Success")
+        guessed_states.append(user_input)
+
 screen.exitonclick()
